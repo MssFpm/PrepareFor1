cb5a41764b3edf25a97602850c49a45ab40aef56
Maxim <afilipko@mail.ru>
2012-12-08 18:27:04 +0300

    Initial Commit

---
+++ PrepareFor1/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PrepareFor1
+//
+//  Created by Maxim on 12/8/12.
+//  Copyright (c) 2012 Maxim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
